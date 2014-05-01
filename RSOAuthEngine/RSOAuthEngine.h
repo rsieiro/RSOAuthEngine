b73111a47f056b28a89cc330888638016ec034f0
Rodrigo Sieiro <rodrigo@sharpcube.com>
2014-05-01 12:20:19 -0300

    Explicitly import MKNetworkEngine

--- RSOAuthEngine/RSOAuthEngine.h
+++ RSOAuthEngine/RSOAuthEngine.h
@@ -23,6 +23,8 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
+#import "MKNetworkEngine.h"
+
 typedef enum _RSOAuthTokenType
 {
     RSOAuthRequestToken,
